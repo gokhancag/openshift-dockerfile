3665065e5152496c5b23e1690c2073096a8cbb0b
Gokhan Cag <gokhan.cag@radity.com>
2023-09-17 00:31:53 +0300

    added files

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Selamlar OpenShift with Dockerfile!"
+
+@app.route('/nasilsin')
+def nasilsin():
+    return 'iyiyim, sen nasilsin?'
+
+@app.route('/hello')
+def hello():
+    return 'hello from OpenShift with Dockerfile..'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
