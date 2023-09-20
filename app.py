88b04ac61c63301402716867b85702ffe3c7d46d
Gokhan CAG <gokhan.cag@radity.com>
2023-09-20 14:18:13 +0300

    added files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Selamlar OpenShift with <b>Dockerfile Test!</b>"
+    return "Selamlar Siemens with <b> Dockerfile!</b>"
 
 @app.route('/nasilsin')
 def nasilsin():
